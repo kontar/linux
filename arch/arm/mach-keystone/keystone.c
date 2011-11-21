c978ccb5b89f49ba7f598a86e3d735abd4d0c2cd
Cyril Chemparathy <cyril@ti.com>
2011-11-21 14:22:47 -0500

    arm: keystone: add keystone machine definitions
    KeystoneII: initial baseport modifications
    
    - setting correct base VA/PA addresses for the debug uart.
      senduart, waituart and busyuart macro taken from mach-davinci code
    
    - irq: setting NR_IRQS 0
      See the "Re: [PATCH] arm: irq: Allow for specification of no preallocated irqs"
      from Russell King
    
    - Initializing iotable to map UART0 address space. Tha is required for
      early_printk.
    
    - gic_handle_irq added to the machine
    
    - using timer64 instead of arch_timer
      The arch_timer does't work on simulator. So we try to use timer64 #16
      as a clock event source
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>

---
+++ arch/arm/mach-keystone/keystone.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2010-2011 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <linux/clk.h>
+#include <linux/clkdev.h>
+#include <linux/io.h>
+#include <linux/irq.h>
+#include <linux/irqdomain.h>
+#include <linux/of.h>
+#include <linux/of_irq.h>
+#include <linux/of_platform.h>
+#include <linux/of_address.h>
+
+#include <asm/unified.h>
+#include <asm/arch_timer.h>
+#include <asm/cacheflush.h>
+#include <asm/hardware/arm_timer.h>
+#include <asm/hardware/timer-sp.h>
+#include <asm/hardware/gic.h>
+#include <asm/hardware/cache-l2x0.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/mach/time.h>
+
+#include <mach/irqs.h>
+
+#include "core.h"
+
+int tmr16_timer_sched_clock_init(void);
+int tmr16_timer_register(void);
+
+static struct map_desc io_desc[] = {
+       {
+               .virtual        = 0xfe800000UL,
+               .pfn            = __phys_to_pfn(0x02000000UL),
+               .length         = 0x800000UL,
+               .type           = MT_DEVICE
+       },
+};
+
+static void __init keystone_map_io(void)
+{
+       iotable_init(io_desc, sizeof(io_desc)/sizeof(struct map_desc));
+}
+
+void keystone_set_cpu_jump(int cpu, void *jump_addr)
+{
+	BUG();
+}
+
+const static struct of_device_id irq_match[] = {
+	{ .compatible = "arm,cortex-a15-gic", .data = gic_of_init, },
+	{}
+};
+
+static void __init keystone_init_irq(void)
+{
+	of_irq_init(irq_match);
+}
+
+
+static void __init keystone_timer_init(void)
+{
+//	arch_timer_of_register();
+//	arch_timer_sched_clock_init();
+	tmr16_timer_register();
+	tmr16_timer_sched_clock_init();
+}
+
+static struct sys_timer keystone_timer = {
+	.init = keystone_timer_init,
+};
+
+
+static void __init keystone_init(void)
+{
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+}
+
+static const char *keystone_match[] __initconst = {
+	"ti,keystone-evm",
+	NULL,
+};
+
+DT_MACHINE_START(KEYSTONE, "Keystone")
+	.map_io		= keystone_map_io,
+	.init_irq	= keystone_init_irq,
+	.timer		= &keystone_timer,
+	.handle_irq	= gic_handle_irq,
+	.init_machine	= keystone_init,
+	.dt_compat	= keystone_match,
+MACHINE_END
