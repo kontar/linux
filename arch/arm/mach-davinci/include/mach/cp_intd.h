96766312a207f57bceeec1a1790db62bf0314b0b
Murali Karicheri <m-karicheri2@ti.com>
2011-11-26 10:27:32 -0500

    tci6614: add driver for cpintd pulse-level convertor
    TCI6614 SOC has CP_INTD (a.k.a Common Platform Interrupt Distributor for
    mapping some of the System interrupts to CP_INTC. This is the driver
    for CP_INTD. This also extend the davinci_soc_info structure so that
    individual SOC can define if there is support for CP_INTD or not. This code
    is designed similar to the CP_INTC driver.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

---
+++ arch/arm/mach-davinci/include/mach/cp_intd.h
@@ -0,0 +1,15 @@
+/*
+ * TI Common Platform Interrupt Distributor (cp_intd) definitions
+ *
+ * Copyright (C) 2011 Texas Instruments
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2. This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+#ifndef __ASM_HARDWARE_CP_INTD_H
+#define __ASM_HARDWARE_CP_INTD_H
+
+void __init cp_intd_init(void);
+
+#endif  /* __ASM_HARDWARE_CP_INTD_H */
