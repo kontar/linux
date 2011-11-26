05cff04233169293ee4ab063488fb601ec3abe4c
Murali Karicheri <m-karicheri2@ti.com>
2011-11-26 10:23:46 -0500

    tci6614: add soc specific definitions
    Adding header file definitions for TCI6614 SoC.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

---
+++ arch/arm/mach-davinci/include/mach/tci6614.h
@@ -0,0 +1,59 @@
+/*
+ * Texas Instruments TCI6614 SoC Specific Defines
+ *
+ * Copyright (C) 2010 Texas Instruments
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __ASM_ARCH_DAVINCI_TCI6614_H
+#define __ASM_ARCH_DAVINCI_TCI6614_H
+
+#include <asm/sizes.h>
+
+#define TCI6614_DDR_BASE	0x80000000
+
+/*
+ * Fixed mapping for early init starts here. If low-level debug is enabled,
+ * this area also gets mapped via io_pg_offset and io_phys by the boot code.
+ * To fit in with the io_pg_offset calculation, the io base address selected
+ * here _must_ be a multiple of 2^21.
+ */
+#define TCI6614_IO_BASE	0x02000000
+
+#define TCI6614_N_GPIO	32
+
+#ifndef __ASSEMBLY__
+
+#include <linux/serial_8250.h>
+
+#include <mach/nand.h>
+#include <mach/serial.h>
+#include <mach/i2c.h>
+#include <mach/spi.h>
+
+
+struct tci6614_device_info {
+	struct davinci_uart_config	*serial_config;
+	struct davinci_nand_pdata	*nand_config[4]; /* 4 chipsels */
+	struct davinci_i2c_platform_data *i2c_config;
+	struct davinci_spi_platform_data *spi_config;
+};
+
+extern struct platform_device tci6614_wdt_device;
+extern struct platform_device tci6614_serial_device;
+
+extern void __init tci6614_init(void);
+extern void __init tci6614_devices_init(struct tci6614_device_info *);
+extern void __init tci6614_irq_init(void);
+extern void __init tci6614_intc_init(void);
+
+#endif
+
+#endif /* __ASM_ARCH_DAVINCI_TCI6614_H */
