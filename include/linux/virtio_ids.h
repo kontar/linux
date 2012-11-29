762d177fc0852b17b0114caff4005828a6fca329
Cyril Chemparathy <cyril@ti.com>
2012-11-28 19:49:31 -0500

    virtio: add udma driver
    virtio: udma: minor fixes
    
    virtio: udma: more fixes
    
    virtio: udma: minor kconfig delta
    
    virtio: udma: minor kconfig delta 2

--- include/linux/virtio_ids.h
+++ include/linux/virtio_ids.h
@@ -38,4 +38,6 @@
 #define VIRTIO_ID_SCSI		8 /* virtio scsi */
 #define VIRTIO_ID_9P		9 /* 9p virtio console */
 
+#define VIRTIO_ID_UDMA		10 /* virtio user-space driver */
+
 #endif /* _LINUX_VIRTIO_IDS_H */
