08eb3738324d076285c71f32375a993e19a8e304
Achin Gupta <achin.gupta@arm.com>
2012-12-17 00:15:00 +0000

    ARM: vexpress: allow native pm ops backends to probe for psci suppport
    This patch allows the vexpress 'tc2' native backend to probe
    the dt for presence of the psci backend. If present then the native
    implementation of the 'bL_platform_power_ops' is not used.
    
    Signed-off-by: Achin Gupta <achin.gupta@arm.com>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>

--- arch/arm/mach-vexpress/tc2_pm.c
+++ arch/arm/mach-vexpress/tc2_pm.c
@@ -25,6 +25,7 @@
 #include <asm/cacheflush.h>
 #include <asm/cputype.h>
 #include <asm/cp15.h>
+#include <asm/psci.h>
 
 #include <linux/arm-cci.h>
 
@@ -315,6 +316,12 @@ static int __init tc2_pm_init(void)
 	void __iomem *scc;
 	u32 a15_cluster_id, a7_cluster_id, sys_info;
 	struct device_node *np;
+
+	ret = psci_probe();
+	if (!ret) {
+		pr_debug("psci found. Aborting native init\n");
+		return -ENODEV;
+	}
 
 	/*
 	 * The power management-related features are hidden behind
