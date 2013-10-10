84ba19d78b6d208eddb7dc7240cbdb93ff61ef9d
Grygorii Strashko <grygorii.strashko@ti.com>
2013-10-10 12:53:45 +0300

    ARM: OMAP4460: cpuidle: WA for ROM bug because of CA9 r2pX gic control register change
    On OMAP4+ devices, GIC register context is lost when MPUSS hits the
    OSWR. On the CPU wakeup path, ROM code gets executed and one of the
    steps in it is to restore the saved context of the GIC.
    
    The ROM code uses GICD != 1 condition to decide how the GIC registers
    are handled in wakeup path from OSWR. But, GICD  register has changed
    between CortexA9 r1pX and r2pX and it contains 2 bits now. Secure view
    which ROM code sees:
          bit 1 == Enable Non-secure
          bit 0 == Enable secure
    Non-secure view which HLOS sees:
          bit 0 == Enable Non-secure
    
    As result, on OMAP4460(r2pX) devices, when the ROM code is executed
    during CPU1 wakeup, GICD == 3 and it fails to understand the real wakeup
    power state and reconfigures GIC distributor to boot values and, as
    result, the entire interrupt controller context will loose in a live
    system.
    
    Hence, implement a workaround on OMAP4460 devices in case if MPUSS has
    hit OSWR - as long as CPU1 sees GICD == 1 in it's wakeup path from OSWR,
    the issue won't happen:
         1.1) CPU0 must disable the GIC distributor, before doing the CPU1
    wakeup,
         1.2) CPU0 should wait until CPU1 will re-enable the GIC distributor
           2) CPU1 must re-enable the GIC distributor on it's wakeup path.
    
    The workaround for CPUIdle has been implemented in the same way as
    for boot-up & hot-plug path in:
     - http://git.kernel.org/?p=linux/kernel/git/tmlind/linux-omap.git;
    a=commitdiff;h=ff999b8a0983ee15668394ed49e38d3568fc6859
    
    For more information see:
    - https://patchwork.kernel.org/patch/1609011/
    - http://www.spinics.net/lists/arm-kernel/msg201402.html
    
    The ROM code bug is applicable to only OMAP4460(r2pX) devices.
    OMAP4470 (also r2pX) is not affected by this bug because code has been
    fixed.
    
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>

--- arch/arm/mach-omap2/cpuidle44xx.c
+++ arch/arm/mach-omap2/cpuidle44xx.c
@@ -80,6 +80,7 @@ static int omap_enter_idle_coupled(struct cpuidle_device *dev,
 			int index)
 {
 	struct idle_statedata *cx = state_ptr + index;
+	bool mpuss_can_lose_context = false;
 
 	/*
 	 * CPU0 has to wait and stay ON until CPU1 is OFF state.
@@ -110,6 +111,9 @@ static int omap_enter_idle_coupled(struct cpuidle_device *dev,
 	 */
 	cpu_pm_enter();
 
+	mpuss_can_lose_context = (cx->mpu_state == PWRDM_POWER_RET) &&
+				 (cx->mpu_logic_state == PWRDM_POWER_OFF);
+
 	if (dev->cpu == 0) {
 		pwrdm_set_logic_retst(mpu_pd, cx->mpu_logic_state);
 		omap_set_pwrdm_state(mpu_pd, cx->mpu_state);
@@ -118,9 +122,8 @@ static int omap_enter_idle_coupled(struct cpuidle_device *dev,
 		 * Call idle CPU cluster PM enter notifier chain
 		 * to save GIC and wakeupgen context.
 		 */
-		if ((cx->mpu_state == PWRDM_POWER_RET) &&
-			(cx->mpu_logic_state == PWRDM_POWER_OFF))
-				cpu_cluster_pm_enter();
+		if (mpuss_can_lose_context)
+			cpu_cluster_pm_enter();
 	}
 
 	omap4_enter_lowpower(dev->cpu, cx->cpu_state);
@@ -128,10 +131,39 @@ static int omap_enter_idle_coupled(struct cpuidle_device *dev,
 
 	/* Wakeup CPU1 only if it is not offlined */
 	if (dev->cpu == 0 && cpumask_test_cpu(1, cpu_online_mask)) {
+		/*
+		 * GIC distributor control register has changed between
+		 * CortexA9 r1pX and r2pX. The Control Register secure
+		 * banked version is now composed of 2 bits:
+		 * bit 0 == Secure Enable
+		 * bit 1 == Non-Secure Enable
+		 * The Non-Secure banked register has not changed
+		 * Because the ROM Code is based on the r1pX GIC, the CPU1
+		 * GIC restoration will cause a problem to CPU0 Non-Secure SW.
+		 * The workaround must be:
+		 * 1) Before doing the CPU1 wakeup, CPU0 must disable
+		 * the GIC distributor and wait until it will be enabled by CPU1
+		 * 2) CPU1 must re-enable the GIC distributor on
+		 * it's wakeup path.
+		 */
+		if (IS_PM44XX_ERRATUM(PM_OMAP4_ROM_SMP_BOOT_ERRATUM_GICD) &&
+		    mpuss_can_lose_context)
+			gic_dist_disable();
+
 		clkdm_wakeup(cpu_clkdm[1]);
 		omap_set_pwrdm_state(cpu_pd[1], PWRDM_POWER_ON);
 		clkdm_allow_idle(cpu_clkdm[1]);
+
+		if (IS_PM44XX_ERRATUM(PM_OMAP4_ROM_SMP_BOOT_ERRATUM_GICD) &&
+		    mpuss_can_lose_context)
+			while (gic_dist_disabled()) {
+				udelay(1);
+				cpu_relax();
+			}
 	}
+
+	if (IS_PM44XX_ERRATUM(PM_OMAP4_ROM_SMP_BOOT_ERRATUM_GICD) && dev->cpu)
+		gic_dist_enable();
 
 	/*
 	 * Call idle CPU PM exit notifier chain to restore
@@ -143,8 +175,7 @@ static int omap_enter_idle_coupled(struct cpuidle_device *dev,
 	 * Call idle CPU cluster PM exit notifier chain
 	 * to restore GIC and wakeupgen context.
 	 */
-	if (dev->cpu == 0 && (cx->mpu_state == PWRDM_POWER_RET) &&
-		(cx->mpu_logic_state == PWRDM_POWER_OFF))
+	if (dev->cpu == 0 && mpuss_can_lose_context)
 		cpu_cluster_pm_exit();
 
 fail:
